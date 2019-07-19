a3d912003c278832bd5da7514a2dc4dfa92a9e12
fengchen <xie_cang@163.com>
2019-07-19 09:44:09 +0800

    增加水管

--- main.js
+++ main.js
@@ -34,30 +34,10 @@ let enableDebugMode = function (game, enable) {
 }
 let __main = function () {
     let images = {
-        bullet: 'img/bullet.png',
-        cloud: 'img/cloud.png',
-        player: 'img/player.png',
-        sky: 'img/sky.png',
-        enemy0: 'img/enemy0.png',
-        enemy1: 'img/enemy1.png',
-        enemy2: 'img/enemy2.png',
-        enemy3: 'img/enemy3.png',
-        enemy4: 'img/enemy4.png',
-        // run
-        run1: 'img/run-01.png',
-        run2: 'img/run-02.png',
-        run3: 'img/run-03.png',
-        run4: 'img/run-04.png',
-        run5: 'img/run-05.png',
-        run6: 'img/run-06.png',
-        // 停止
-        idle1: 'img/idle-00.png',
-        idle2: 'img/idle-01.png',
-        idle3: 'img/idle-02.png',
-        idle4: 'img/idle-03.png',
         // 背景
         cave: 'img/bg_day.png',
         ground: 'img/land.png',
+        pipe: 'img/pipe_up.png',
 
         bird1: 'img/bird1_0.png',
         bird2: 'img/bird1_1.png',
--- scene/title/title.js
+++ scene/title/title.js
@@ -1,3 +1,61 @@
+class Pipes {
+    constructor(game) {
+        this.game = game
+        this.pipes = []
+        // 管子竖向间距
+        this.pipeSpace = 150
+        // 管子横向间距
+        this.pipeSpaceX = 200
+        this.columsOfPipe = 3
+        for (let i = 0; i < this.columsOfPipe; i++) {
+            let p1 = GuaImage.new(game, 'pipe')
+            p1.flipY = true
+            p1.x = 500 + i * 200
+            let p2 = GuaImage.new(game, 'pipe')
+            p2.x = p1.x
+            this.resetPipesPosition(p1, p2)
+            this.pipes.push(p1)
+            this.pipes.push(p2)
+        }
+    }
+    static new(game) {
+        return new this(game)
+    }
+    resetPipesPosition(p1, p2) {
+        p1.y = randomBetween(-280, 0)
+        p2.y = p1.y + p1.h + this.pipeSpace
+    }
+    update() {
+        for (let p of this.pipes) {
+            p.x -= 5
+            if (p.x < -100) {
+                log(p.x)
+                p.x += this.pipeSpaceX * this.columsOfPipe
+                log(p.x)
+            }
+        }
+    }
+    draw() {
+        // this.game.drawImage(this)
+        let context = this.game.context
+        for (let p of this.pipes) {
+            context.save()
+            // 图片角度旋转
+            let w2 = p.w / 2
+            let h2 = p.h / 2
+            context.translate(p.x + w2, p.y + h2)
+            let scaleX = p.flipX ? -1 : 1
+            let scaleY = p.flipY ? -1 : 1
+            context.scale(scaleX, scaleY)
+            context.rotate(p.rotation * Math.PI / 180)
+            context.translate(-w2, -h2)
+            context.drawImage(p.texture, 0, 0)
+
+            context.restore()
+        }
+    }
+}
+
 class SceneTitle extends GuaScene {
     constructor(game) {
         super(game)
@@ -10,11 +68,10 @@ class SceneTitle extends GuaScene {
         let cave = GuaImage.new(game, 'cave')
         this.addElement(cave)
 
-        // let g = GuaImage.new(game, 'ground')
-        // g.x = 0
-        // g.y = 450
-        // this.g = g
-        // this.addElement(g)
+        // 加入水管
+        this.pipe = Pipes.new(game)
+        this.addElement(this.pipe)
+
         this.grounds = []
         for (let i = 0; i < 3; i++) {
             let g = GuaImage.new(game, 'ground')
